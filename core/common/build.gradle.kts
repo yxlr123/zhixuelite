27f85a46f103fc335e6a6bdd6d0ceab6e90a8d83
luiooiul <mail@luiooiul.com>
2024-05-25 09:52:21 +0800

    3

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.zhixuelite.android.library)
+    alias(libs.plugins.zhixuelite.android.hilt)
+}
+
+android {
+    namespace = "com.zhixue.lite.core.common"
+}
---
+++ core/common/src/main/kotlin/com/zhixue/lite/core/common/json/JsonQualifier.kt
@@ -0,0 +1,11 @@
+package com.zhixue.lite.core.common.json
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class NetworkJson
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PreferencesJson
